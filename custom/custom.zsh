6a576f85934c2a07f92cafd387d565ab388c456b
Nopanun Laochunhanun <nopanun@live.com>
2021-08-27 22:21:32 +0700

    custom: add comment and update

--- custom/custom.zsh
+++ custom/custom.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
-
-alias zshconfig="$EDITOR ~/.zshrc"
-alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
+# e=emacsclient.sh --no-wait
+# $EDITOR=emacsclient.sh
+# Use $EDITOR when editing result is piping with other commands
+alias zshrc="e ~/.zshrc"
+alias omz="e ~/.oh-my-zsh"
 alias av=alias_value
-alias e=$EDITOR
 alias zc="e $ZSH_CUSTOM"
