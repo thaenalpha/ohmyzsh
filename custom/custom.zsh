651148d59b8ff1cad9fb7925d739ec5b1deaa417
Nopanun Laochunhanun <nopanun@live.com>
2021-10-15 23:05:40 +0700

    fix(custom): remove omz alias (duplicate with cli)

--- custom/custom.zsh
+++ custom/custom.zsh
@@ -3,7 +3,6 @@
 # $EDITOR=emacsclient.sh
 # Use $EDITOR when editing result is piping with other commands
 alias zshrc="e ~/.zshrc"
-alias omz="e ~/.oh-my-zsh"
 alias av=alias_value
 alias zc="e $ZSH_CUSTOM"
 alias open=xdg-open
