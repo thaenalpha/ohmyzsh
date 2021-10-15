317fdac930773700834337fb1e6344ceababf293
Nopanun Laochunhanun <nopanun@live.com>
2021-10-15 20:58:00 +0700

    fix(Windows):use fullpath,feat(clojure-apps):email

--- custom/clojure-apps.zsh
+++ custom/clojure-apps.zsh
@@ -1,3 +1,4 @@
 randpm() {(~/randpm && bb clojure -M:rand-pm)}
 rand-poshthemes() {(~/randpm && bb clojure -M:rand-ompth)}
 rand-ua() {(~/randpm && bb clojure -X:rand-ua)}
+email() { ls ~/Mail | bb -i '(rand-nth (conj *input* "nopanun@pm.me"))' }
