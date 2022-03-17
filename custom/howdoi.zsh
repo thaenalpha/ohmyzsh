576d498e94978076acf7be60784ca58726eceb9f
Nopanun Laochunhanun <nopanun@live.com>
2022-03-17 11:19:39 +0700

    fix,feat(custom): remove h alias from howdoi, add `hcook`

--- custom/howdoi.zsh
+++ custom/howdoi.zsh
@@ -1,2 +1,4 @@
-alias h='function hdi(){ howdoi $* -c -n 5; }; hdi'
-hl() { howdoi "$*" -ac | less --raw-control-chars --quit-if-one-screen --no-init; }
+function hdi(){ howdoi $* -c -n 5; }
+alias hless='function hl(){ howdoi $* -c | less --raw-control-chars --quit-if-one-screen --no-init; }; hl'
+alias hl=hless
+alias hcook='function hcook(){ HOWDOI_URL=cooking.stackexchange.com howdoi $* ; }; hcook'
